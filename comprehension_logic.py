b8e95048ae3c86f9a6bd57186c409aae19b30160
sayan <sayantankar01@gmail.com>
2022-09-22 14:07:26 +0530

    Initial commit

--- comprehension_logic.py
+++ comprehension_logic.py
@@ -1,6 +1,6 @@
 a = [i for i in range(10) if i % 2 == 1]
 print(a)
 
-c = 1;
-b = [j for j in range(10) c*=j]
-print(b)
+# c = 1;
+# b = [j * b[-1] for j in range(1, 11)]
+# print(b)
--- demo_01.py
+++ demo_01.py
@@ -11,19 +11,19 @@
 #     a = a + (c,)
 #
 # odd(c)
-
-
-def fact(*t):
-    w = 1
-    for i in t:
-        w *= i
-    print(w)
-
-
-a = ()
-n = int(input("Enter the range of list: "))
-for i in range(0, n):
-    c = int(input("Enter the number: "))
-    a = a + (c,)
-
-fact(a)
+#
+#
+# def fact(*t):
+#     w = 1
+#     for i in t:
+#         w *= i
+#     print(w)
+#
+#
+# a = ()
+# n = int(input("Enter the range of list: "))
+# for i in range(0, n):
+#     c = int(input("Enter the number: "))
+#     a = a + (c,)
+#
+# fact(a)
